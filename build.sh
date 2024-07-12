2cd8e7183a05dc260ab5ac3e6fb9245eac626811
Boyquotes <webmaster@linux-live-cd.org>
2024-07-12 12:48:35 +0000

    better build

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 rm -rf ../exampleDist
 vite build -c vercelVite.config.js
 cp -r example/exampleDist ../exampleDist
+git stash
 git checkout buildGithub
 #rm -rf ./
 cp -r ../exampleDist/* ./
+git add .
+git commit -m"update"
+git push
 git checkout main
