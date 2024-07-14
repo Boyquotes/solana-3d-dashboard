aa2a0c0defd08fe431f4fb7455ee5d2b18ed3292
Boyquotes <webmaster@linux-live-cd.org>
2024-07-14 21:41:06 +0000

    before update

--- example/SonarWatch.tsx
+++ example/SonarWatch.tsx
@@ -14,10 +14,12 @@ export default function SonarWatch() {
 
     const [loading, setLoading] = useState(false);
     const [reload, setReload] = useState(false);
-    const [counterSound, setCounterSound] = useState();
+    const [nbToken, setNbToken] = useState(0);
+    const [middleWallet, setMiddleWallet] = useState(0);
     const [dataSonarWatch, setDataSonarWatch] = useState();
     const [cubeNetworthText, setCubeNetworthText] = useState([]);
     const [cubeImgToken, setCubeImgToken] = useState([]);
+    const [newTextWallet, setNewTextWallet] = useState([]);
     let [positionX, setPositionX] = useState(0);
     // AUDIO
     const listener = new THREE.AudioListener();
@@ -25,13 +27,13 @@ export default function SonarWatch() {
     const sound = new THREE.Audio( listener );
 
     // SOLANA
-    const [addressWalletSolana, setAddressWalletSolana] = useState("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
+    const [addressWalletSolana, setAddressWalletSolana] = useState();
     //   setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
     const [balance, setBalance] = useState(null);
 
     const provider = window.solana;
-    console.log("provider");
-    console.log(provider);
+    // console.log("provider");
+    // console.log(provider);
     // const connectWallet = async () => {
         // try {
         //     if (provider && provider.isPhantom) {
@@ -70,8 +72,11 @@ export default function SonarWatch() {
     }
 
     useEffect(() => {
-        const fetchDataSonar = async () => {
+        const fetchDataSonar = async (address=null) => {
             setDataSonarWatch('');
+            if(address && !addressWalletSolana){
+                setAddressWalletSolana(address);
+            }
         console.log(addressWalletSolana+" addressWalletSolana");
             const apiUrl = "https://portfolio-api.sonar.watch/v1/portfolio/fetch?useCache=false&address=" + addressWalletSolana + "&addressSystem=solana"
             try {
@@ -87,50 +92,51 @@ export default function SonarWatch() {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const data = await response.json();
-                console.log("data sonar");
-                console.log(data);
-                console.log(data.fetcherReports);
-                console.log(data.owner);
-                console.log(data.tokenInfo);
+                // console.log("data sonar");
+                // console.log(data);
+                // console.log(data.fetcherReports);
+                // console.log(data.owner);
+                // console.log(data.tokenInfo);
                 if(data.tokenInfo.length == 0){
                     console.log("empty wallet");
                 }
-                setDataSonarWatch(data);
+                // setDataSonarWatch(data);
+                setNbToken(0);
+                displayDataSonar(data);
                 setLoading(false);
                 setReload(false);
             } catch (error) {
+                setLoading(false);
                 console.error('Error fetching data:', error);
             }
         };
         fetchDataSonar();
     }, [addressWalletSolana, reload]);
 
-    const displayDataSonar = async () => {
+    const displayDataSonar = async (data) => {
             const newArray = [];
             setCubeImgToken(newArray);
             // debugger
             let i = 0;
             let z = 0
             let imgToken;
-            dataSonarWatch.elements.forEach(element => {
+            console.log(data)
+            
+            data.elements.forEach(element => {
                 console.log(element)
                 if (element.platformId == "wallet-tokens") {
+                    console.log("element.platformId"+element.platformId);
                     if (element.value > 0.0001) {
-                        console.log(positionX);
                         let netWorth = element.value.toFixed(2);
                         console.log('aquis' + netWorth);
                         let tokens = element.data.assets;
                         console.log(tokens);
-                        console.log("tokens.length "+tokens.length);
-                        if(tokens.length > 10){
-                            let nbLine = Math.round(tokens.length / 10);
-                            console.log("nbLine"+nbLine);
-                        }
-
+                        let tokenWorth = 0;
                         tokens.forEach(tokenSW => {
-                            console.log(tokenSW.data.address);
-                            console.log(tokenSW.data.amount);
-                            console.log(tokenSW);
+                            setNbToken(nbToken+1);
+                            // console.log(tokenSW.data.address);
+                            // console.log(tokenSW.data.amount);
+                            // console.log(tokenSW);
                             if(tokenSW.data.address == "11111111111111111111111111111111"){
                                 imgToken = "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png";
                             }
@@ -146,10 +152,16 @@ export default function SonarWatch() {
                             else{
                                 netWorth = tokenSW.value.toFixed(4);
                             }
+                            if (tokenSW.data.amount > 1) {
+                                tokenWorth = tokenSW.data.amount.toFixed(2);
+                            }
+                            else{
+                                tokenWorth = tokenSW.data.amount.toFixed(4);
+                            }
                             console.log(imgToken);
                             const newImgToken = (
                                 <Suspense fallback={null}>
-                                    <RigidBody mass={0.4} position={[i, 2, z]}>
+                                    <RigidBody mass={0.2} position={[i, 2, z]}>
                                         <BoxWithTexture url={imgToken} />
                                     </RigidBody>
                                 </Suspense>
@@ -164,18 +176,27 @@ export default function SonarWatch() {
                                     fontSize={0.5}
                                     textAlign="center"
                                 >
-                                    {netWorth}
+                                    {netWorth}$
                                 </Text>);
-                            console.log(i);
+                            const newTextTokenWorth = (
+                                <Text
+                                    rotation={[0, Math.PI, 0]}
+                                    position={[i, 1.5, z]}
+                                    color="green"
+                                    fontSize={0.5}
+                                    textAlign="center"
+                                >
+                                    {tokenWorth}
+                                </Text>);
                             const newRigidB = (
                                 <group position={[i, 0, 0]}>
                                     {newText}
+                                    {newTextTokenWorth}
                                     {newImgToken}
                                 </group>
                             )
                             setCubeImgToken((prevMeshes) => [...prevMeshes, newRigidB]);
                             setCubeNetworthText((prevMeshes) => [...prevMeshes, newText]);
-                            console.log(i);
                             i = i+3;
                             if(i<10){
                                 playMoneySound();
@@ -185,7 +206,6 @@ export default function SonarWatch() {
                                 z += 10;
                                 playMoneySound();
                             }
-                            console.log(i);
                         });
                     }
                     else{
@@ -195,13 +215,14 @@ export default function SonarWatch() {
             });
     };
 
-    useEffect(() => {
-        console.log("data reload")
-        displayDataSonar();
-    }, [dataSonarWatch]);
+    // useEffect(() => {
+    //     console.log("data reload")
+    //     displayDataSonar();
+    // }, [dataSonarWatch]);
 
     useEffect(() => {
         const getWalletSolana = async () => {
+            let newTextWallet;
             try {
                 if (provider && provider.isPhantom) {
                     // Connect to the wallet
@@ -219,13 +240,26 @@ export default function SonarWatch() {
                     // Convert the balance from lamports to SOL (1 SOL = 1,000,000,000 lamports)
                     const balanceInSOL = balance / LAMPORTS_PER_SOL;
                     console.log(`Balance: ${balanceInSOL} SOL`);
+                    newTextWallet = (<Text
+                        rotation={[0, Math.PI, 0]}
+                        position={[0, 4, 0]}
+                        color="black"
+                        fontSize={0.5}
+                        onClick={checkYourAddress}
+                        >View your portfolio {cubeImgToken.length}
+                        </Text>);
                 }
                 else{
-                    console.log("pas de provider");
-                    setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
-                    console.log(addressWalletSolana)
-                    console.log("addressWalletSolana")
+                    newTextWallet = (<Text
+                        rotation={[0, Math.PI, 0]}
+                        position={[0, 4, 0]}
+                        color="black"
+                        fontSize={0.5}
+                        onClick={checkYourAddress}
+                        >You don't have a phantom wallet!
+                        </Text>);
                 }
+                setNewTextWallet(newTextWallet);
             } catch (err) {
                 console.error(err);
             }
@@ -249,63 +283,84 @@ export default function SonarWatch() {
     //     sound.play();
     // });
 
-  const checkBalancesAddressTest = () => {
-    // Prevent the default behavior
-    setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S")
-    if( addressWalletSolana == "GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S"){
-        console.log("event deja display reload");
-        setLoading(false);
-        setAddressWalletSolana('');
-        setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
-        setReload(true);
-    }
+  const checkYourAddress = () => {
     setCubeImgToken([]);
-    console.log(event);
     if(loading){
         console.log("already onDemand");
     }
     else{
+        setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S")
         setLoading(true);
-        // fetchDataSonar();
+        setReload(true);
+        // fetchDataSonar("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
+    }
+  };
+
+  const checkBalancesAddressTest = () => {
+    setCubeImgToken([]);
+    if(loading){
+        console.log("already onDemand");
+    }
+    else{
+        setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S")
+        setLoading(true);
+        setReload(true);
+        // fetchDataSonar("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
     }
   };
 
     const checkBalancesAddress  = () => {
-        setAddressWalletSolana("Zztedt4LkJ98H1gosbMe554Pas95Jop3khBiyTSDGzS")
+        setCubeImgToken([]);
         if(loading){
             console.log("already onDemand");
         }
         else{
-            setCubeImgToken([]);
+            setAddressWalletSolana("MJKqp326RZCHnAAbew9MDdui3iCKWco7fsK9sVuZTX2")
             setLoading(true);
             setReload(true);
-            // fetchDataSonar("Zztedt4LkJ98H1gosbMe554Pas95Jop3khBiyTSDGzS");
+            // fetchDataSonar("MJKqp326RZCHnAAbew9MDdui3iCKWco7fsK9sVuZTX2");
         }
     };
  console.log("address solana"+addressWalletSolana);
  console.log("cubeImgToken"+cubeImgToken.length);
-    return (
+// setMiddleWallet(0);
+let lengthWallet, calculMiddleWallet = 0;
+console.log("tokens.length "+cubeImgToken.length);
+if(cubeImgToken.length > 10){
+    let nbLine = Math.round(cubeImgToken.length / 10);
+    console.log("nbLine"+nbLine);
+    lengthWallet = (10 * 2) + ((10-1) *3);
+}
+else{
+    lengthWallet = ((cubeImgToken.length) * 2) + ((cubeImgToken.length-1) *3);
+}
+
+console.log("length wallet "+lengthWallet);
+calculMiddleWallet = lengthWallet / 2;
+console.log("middleWallet wallet "+calculMiddleWallet);
+// setMiddleWallet(calculMiddleWallet);
+ return (
         <>
             <group position={[0, 0, 10]}>
-                <TextInput3D />
+                {newTextWallet}
                 <Text
                 rotation={[0, Math.PI, 0]}
-                position={[-3.5, 7, 0]}
+                position={[-3.5, 5, 0]}
                 color="black"
                 fontSize={0.5}
                 onClick={checkBalancesAddress}
-                >View your portfolio {cubeImgToken.length}
+                >View whale portfolio {cubeImgToken.length}
                 </Text>
                 <Text
                 rotation={[0, Math.PI, 0]}
-                position={[3.5, 7, 0]}
+                position={[3.5, 5, 0]}
                 color="black"
                 fontSize={0.5}
                 onClick={checkBalancesAddressTest}
                 >View test wallet
                 </Text>
             </group>
-            <group position={[-5, 0, 10]}>
+            <group position={[-calculMiddleWallet, 0, 10]}>
                 {cubeImgToken.map((item, i, arr) => {
                 // if (arr.length - 1 === i) {
                     // console.log(item)
